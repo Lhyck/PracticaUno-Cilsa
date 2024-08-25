3b583fcf63c499eb4473f25b31fa5bb91a35a8f7
Lhyck <3ksablanke@gmail.com>
2024-08-25 16:27:09 -0300

    mejorando

---
+++ main.js
@@ -0,0 +1,50 @@
+
+function onClick(event) {
+    event.preventDefault();
+   // this.style.backgroundColor = "salmon";
+    console.log("click ...");
+    console.log(event);
+
+    const mensaje = {
+
+        nombre: document.getElementById('nombre').value,
+        celular: document.getElementById('apellido').value,
+        fechaDeNacimiento: document.getElementById('fechaDeNacimiento').value,
+        paisDeResidencia: document.getElementById('paisDeResidencia').value,
+        email: document.getElementById('email').value,
+      
+
+    }
+    console.log(mensaje);
+
+
+    fetch('https://jsonplaceholder.typicode.com/posts', {
+            method: "POST",
+            body: JSON.stringify(mensaje),
+            headers: { "Content-type": "application/json; charset" }
+        })
+        .then((response) => response.json())
+        .then((json) => {
+            console.log(json);
+            Swal.fire(
+                ' Su registro se ha realizado con exito ... ',
+                ' Lo contactaremos a la brevedad '
+
+            );
+            cleanForm();
+
+        })
+        .catch((err) => console.log(err));
+
+    function cleanForm() {
+        let formulario = document.getElementById('form1');
+        formulario.reset();
+    }
+
+    function redirectUrl() {
+        window.location.href = "https://google.com";
+    }
+
+}
+let boton = document.getElementById("enviar");
+boton.addEventListener("click", onClick);
